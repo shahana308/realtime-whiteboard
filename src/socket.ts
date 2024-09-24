6f3eb3f1da2ec040edca1e2846c62a9ecacecf9d
shahana <shahana.salam@cafu.com>
2024-09-24 14:32:05 +0400

    fix: cors issue

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io(process.env.REACT_APP_URL || "http://localhost:4000");
 
 export default socket;
