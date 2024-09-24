f3876a9105d9c068cb9e7d1a3f345487152e034a
shahana <shahana.salam@cafu.com>
2024-09-24 15:51:13 +0400

    test

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_URL || "http://localhost:4000");
+const socket = io(process.env.REACT_APP_URL || "http://localhost:4001");
 
 export default socket;
