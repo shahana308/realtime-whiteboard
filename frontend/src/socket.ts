ebb1ed4c024778e7c5adeb6c15381b5d0940d905
shahana <shahana.salam@cafu.com>
2024-09-25 11:18:02 +0400

    test

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,5 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_BACKEND_URL || "http://localhost:4000");
+const socket = io(
+  process.env.REACT_APP_BACKEND_URL ||
+    "https://realtime-whiteboard-l4cw.onrender.com",
+  {
+    transports: ["websocket"], // Force WebSocket
+  }
+);
 
 export default socket;
